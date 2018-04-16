ba31ce80460736426a54974ab16687d954a8de36
Lucie Tauchenova <tauchenl@students.zcu.cz>
2018-04-16 16:08:57 +0200

    Folder for Unit tests added

---
+++ SSCISTest/Class/SSCISSessionManagerTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SSCIS.Class;
+using SSCISTest.Class.TestObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SSCISTest.Class
+{
+    [TestClass]
+    public class SSCISSessionManagerTest
+    {
+        [TestMethod]
+        public void TestSessionDestroy()
+        {
+            SSCISSessionManager session = new SSCISSessionManager();
+
+            HttpSessionStateBaseTO httpSession = new HttpSessionStateBaseTO();
+            httpSession["sessionId"] = 1;
+            httpSession["role"] = 2;
+            httpSession["hash"] = "34355677887988533";
+            httpSession["userID"] = 1234;
+
+            session.SessionDestroy(1, httpSession);
+
+            Assert.IsFalse(httpSession.ContainsKey("sessionId"));
+            Assert.IsFalse(httpSession.ContainsKey("role"));
+            Assert.IsFalse(httpSession.ContainsKey("hash"));
+            Assert.IsFalse(httpSession.ContainsKey("userID"));
+        }
+    }
+}
---
+++ SSCISTest/Class/TestObjects/HttpSessionStateBaseTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SSCISTest.Class.TestObjects
+{
+    public class HttpSessionStateBaseTO : HttpSessionStateBase
+    {
+        public HttpSessionStateBaseTO()
+        {
+        }
+        public bool ContainsKey(object key)
+        {
+            foreach (var k in Keys)
+            {
+                if (k.Equals(key)) return true;
+            }
+            return false;
+        }
+    }
+}
