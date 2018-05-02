b933805d5cb3db3a8c36a2ea82ac0559fb75642a
Filip Hácha <hachaf@students.zcu.cz>
2018-05-02 20:35:55 +0200

    ApplicationSubjectsResolver and SSCISHtml UnitTests

---
+++ SSCISTest/Class/ApplicationSubjectsResolverTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SSCIS.Class;
+using SSCIS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSCISTest.Class
+{
+    [TestClass]
+    public class ApplicationSubjectsResolverTest
+    {
+        [TestMethod]
+        public void TestResolveSubjects()
+        {
+            ApplicationSubjectsResolver resolver = new ApplicationSubjectsResolver();
+
+            Mock<SSCISEntities> dbMock = new Mock<SSCISEntities>();
+            dbMock.Setup(m => m.Subject.Find(1)).Returns(new Subject() { ID = 1 });
+            dbMock.Setup(m => m.Subject.Find(3)).Returns(new Subject() { ID = 3 });
+            dbMock.Setup(m => m.Subject.Find(5)).Returns(new Subject() { ID = 5 });
+
+            List<TutorApplicationSubject> subjects = resolver.ResolveSubjects("1 3 5;2 4 1", dbMock.Object);
+
+            Assert.AreEqual(1, subjects[0].Subject.ID);
+            Assert.AreEqual(3, subjects[1].Subject.ID);
+            Assert.AreEqual(5, subjects[2].Subject.ID);
+
+            Assert.AreEqual(byte.Parse("2"), subjects[0].Degree);
+            Assert.AreEqual(byte.Parse("4"), subjects[1].Degree);
+            Assert.AreEqual(byte.Parse("1"), subjects[2].Degree);
+        }
+    }
+}
---
+++ SSCISTest/Class/SSCISHtmlTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SSCIS.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSCISTest.Class
+{
+    [TestClass]
+    public class SSCISHtmlTest
+    {
+        [TestMethod]
+        public void TestDisplayForBool()
+        {
+            Assert.AreEqual("", SSCISHtml.DisplayForBool(null));
+            Assert.AreEqual("Ano", SSCISHtml.DisplayForBool(true));
+            Assert.AreEqual("Ne", SSCISHtml.DisplayForBool(false));
+        }
+    }
+}
