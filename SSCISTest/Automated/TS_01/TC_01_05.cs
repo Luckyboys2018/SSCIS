28111f8901ce6755428a673fbe00354f2f97403c
Filip Hácha <hachaf@students.zcu.cz>
2018-05-10 19:18:37 +0200

    TC.01.05

---
+++ SSCISTest/Automated/TS_01/TC_01_05.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSCISTest.Automated.TS_01
+{
+    [TestClass]
+    public class TC_01_05
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            driver = new ChromeDriver();
+            baseURL = "https://www.katalon.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TestCleanup]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [TestMethod]
+        public void TheTC0105Test()
+        {
+            driver.Navigate().GoToUrl("http://ourea98.zcu.cz/");
+            driver.FindElement(By.LinkText("Přihlásit se")).Click();
+            driver.FindElement(By.Id("Login")).Click();
+            driver.FindElement(By.Id("Login")).Clear();
+            driver.FindElement(By.Id("Login")).SendKeys("ADMIN_TST_00");
+            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+            driver.FindElement(By.LinkText("Žádosti tutorů")).Click();
+            driver.FindElement(By.LinkText("Detail")).Click();
+            driver.FindElement(By.LinkText("Schválit")).Click();
+            Assert.AreEqual("Přihlášky tutorů", driver.FindElement(By.XPath("//h1")).Text);
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
